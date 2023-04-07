b610bc4704995bbafda5b5488be2784789409208
Brandon <brandonluton8@gmail.com>
2023-04-07 12:39:56 -0400

    updated maps

--- maps/map.js
+++ maps/map.js
@@ -3,23 +3,30 @@ function createMap()
     const map = L.map('map', {
         center: [39.299236, -76.609383],
         zoom: 12,
-        //scrollWheelZoom:false
+        scrollWheelZoom:false
         });
 
     // define different layers, set default to StreetView
     // Todo: different/better layers?
     const basemaps = {
             StreetView: L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',   {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}),
-            Topography: L.tileLayer.wms('http://ows.mundialis.de/services/service?',   {layers: 'TOPO-WMS'}),
-            Places: L.tileLayer.wms('http://ows.mundialis.de/services/service?', {layers: 'OSM-Overlay-WMS'})
-        };
-    L.control.layers(basemaps).addTo(map);
+            //Topography: L.tileLayer.wms('http://ows.mundialis.de/services/service?',   {layers: 'TOPO-WMS'}),
+            Places: L.tileLayer.wms('http://ows.mundialis.de/services/service?', {layers: 'OSM-Overlay-WMS', attribution: '&copy; <a href="https://www.terrestris.de/de/openstreetmap-wms/">terrestris</a> contributors'}),
+            Satellite: L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{subdomains:['mt0','mt1','mt2','mt3'], attribution: '&copy; <a href= "https://cloud.google.com/maps-platform/terms/">Google Maps</a> contributors'})
+            //Terrain: L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',{subdomains:['mt0','mt1','mt2','mt3']}),
+            //Hybrid: L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',{maxZoom: 20,subdomains:['mt0','mt1','mt2','mt3']}),
+            //Streets: L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{maxZoom: 20,subdomains:['mt0','mt1','mt2','mt3']})
+    };
+    L.control.layers(basemaps, null, {position: 'bottomleft'}).addTo(map);
     basemaps.StreetView.addTo(map)
 
     // reset map to default location
     L.easyButton('fa-home',function(btn,map){
         map.setView([39.299236, -76.609383], 12);
       }).addTo(map);
+
+    // scale
+    L.control.scale({position: 'bottomright'}).addTo(map);
 
     return map;
 }
